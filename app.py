a02867d8fddba854a8314aee96c0068d1b73fd87
shernandez33 <shernandezramos33@gmail.com>
2026-04-29 08:17:39 -0400

    v1.2: Refactorización y mejora de la lógica

--- app.py
+++ app.py
@@ -1,4 +1,40 @@
 print("Hola Mundo, Git y GitHub, ensayo de laboratorio")
+
 def suma(s1:int, s2:int)-> int:
     print(f"La suma es: {s1+s2}")
+
+
+# Primer ensayo con dataclasses en python3.
+
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    username: str
+    login: str
+    passwd: str
+    email: str
+
+
+if __name__ == "__main__":
+    
+    cody = User("Sergio Hernández","shernandez","abcd567", "shernandez@gmail.com")
+    
+    print(cody.username)
+    print(cody.login)
+    print(cody.passwd)
+    print(cody.email)
+    
+    print("Cambiando valores de los atributos...")
+    
+    cody.username = "Sergio L. Hernández"
+    cody.login = "shernandez33"
+    cody.passwd = "abCD1234+"
+    cody.email = "shernandezramos33@gail.com"
+    
+    print(cody.username)
+    print(cody.login)
+    print(cody.passwd)
+    print(cody.email)
+    
 
